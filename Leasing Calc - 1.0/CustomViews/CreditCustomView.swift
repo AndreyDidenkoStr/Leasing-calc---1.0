c614815a90c042aad2fed49e246629e17816d320
AndreyDidenkoStr <DidenkoAndreyStr@gmail.com>
2022-08-27 11:18:13 +0300

    Ui in custom

--- Leasing Calc - 1.0/CustomViews/CreditCustomView.swift
+++ Leasing Calc - 1.0/CustomViews/CreditCustomView.swift
@@ -18,5 +18,64 @@ final class CreditCustomView: UIView {
         fatalError("init(coder:) has not been implemented")
     }
     
+    // - full price
+    // - first payment
+    // - month
+    // - rate
     
+    // - Labels
+    lazy var priceLabel: UILabel = {
+        let label = UILabel()
+        
+        return label
+    }()
+    
+    lazy var firstPaymentLabel: UILabel = {
+        let label = UILabel()
+        
+        return label
+    }()
+    
+    lazy var monthLabel: UILabel = {
+        let label = UILabel()
+        
+        return label
+    }()
+    
+    lazy var rateLabel: UILabel = {
+        let label = UILabel()
+        
+        return label
+    }()
+    
+    // - TextFields
+    lazy var priceTextField: UITextField = {
+        let tf = UITextField()
+        
+        return tf
+    }()
+    
+    lazy var firstPaymentTextField: UITextField = {
+        let tf = UITextField()
+        
+        return tf
+    }()
+    
+    lazy var monthTextField: UITextField = {
+        let tf = UITextField()
+        
+        return tf
+    }()
+    
+    lazy var rateTextField: UITextField = {
+        let tf = UITextField()
+        
+        return tf
+    }()
+    
+    // setup UI and constraint func
+    
+    private func setupUI() {
+        
+    }
 }
